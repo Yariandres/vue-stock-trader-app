ab573ed7247042677b0fbdddcde8fa77995b500c
Yari Andres <yari.andres@gmail.com>
2021-01-29 22:08:19 +0100

    connecting portfolio stocks

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="col-sm-6 col-md-4">
-		<div class="panel panel-success">
+		<div class="panel panel-info">
 			<div class="panel-heading">
 				<h3 class="panel-title">
 					{{ stock.name }}
 					<small
-						>(price: {{ stock.price }} / Quantity:
-						{{ stock.quatity }})</small
+						>(Price: {{ stock.price }} | Quantity:
+						{{ stock.quantity }})</small
 					>
 				</h3>
 			</div>
@@ -17,6 +17,7 @@
 						class="form-control"
 						placeholder="Quantity"
 						v-model="quantity"
+						:class="{ danger: insufficientQuantity }"
 					/>
 				</div>
 				<div class="pull-right">
@@ -24,36 +25,52 @@
 						class="btn btn-success"
 						@click="sellStock"
 						:disabled="
-							quantity <= 0 || !Number.isInteger(+quantity)
+							insufficientQuantity ||
+							quantity <= 0 ||
+							!Number.isInteger(quantity)
 						"
 					>
-						Sell
+						{{ insufficientQuantity ? 'Not enough' : 'Sell' }}
 					</button>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+
+<style scoped>
+	.danger {
+		border: 1px solid red;
+	}
+</style>
+
 <script>
 	import { mapActions } from 'vuex';
+
 	export default {
 		props: ['stock'],
-
 		data() {
 			return {
 				quantity: 0,
 			};
 		},
+		computed: {
+			insufficientQuantity() {
+				return this.quantity > this.stock.quantity;
+			},
+		},
 		methods: {
-			...mapActions(['sellStock']),
-
+			...mapActions({
+				placeSellOrder: 'sellStock',
+			}),
 			sellStock() {
 				const order = {
 					stockId: this.stock.id,
 					stockPrice: this.stock.price,
 					quantity: this.quantity,
 				};
-				this.sellStock();
+				this.placeSellOrder(order);
+				this.quantity = 0;
 			},
 		},
 	};
--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -4,43 +4,43 @@ const state = {
 };
 
 const mutations = {
-	'BUY_STOCK'(state, { stockId, quantity, stockPrice }) {
-		const record = state.stocks.find(element => element.id === stockId);
-
-		if (record) {
-			record.quantity += quantity;
-		} else {
-			state.stocks.push({
-				id: stockId,
-				quantity: quantity,
-			});
-		}
-
-		state.funds -= stockPrice * quantity;
-	},
-	'SELL_STOCK'(state, { stockId, quantity, stockPrice }) {
-		const record = state.stocks.find(element => element.id === stockId);
-
-		if (record.quantity > quantity) {
-			record.quantity -= quantity;
-		} else {
-			state.stocks.splice(state.stocks.indexOf(record), 1);
-		}
-
-		state.funds += stockPrice * quantity;
-	},
+    'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record) {
+            record.quantity += quantity;
+        } else {
+            state.stocks.push({
+                id: stockId,
+                quantity: quantity
+            });
+        }
+        state.funds -= stockPrice * quantity;
+    },
+    'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+        state.funds += stockPrice * quantity;
+    },
+    // 'SET_PORTFOLIO' (state, portfolio) {
+    //     state.funds = portfolio.funds;
+    //     state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : [];
+    // }
 };
 
 const actions = {
-	seelStock({ commit }, order) {
-		commit('SELL_STOCK', order);
-	},
+    sellStock({commit}, order) {
+        commit('SELL_STOCK', order);
+    }
 };
 
 const getters = {
-	stockPortfolio(state, getters) {
+    stockPortfolio (state, getters) {
         return state.stocks.map(stock => {
-            const record = getters.stocks.find(element => element.id === stock.id);
+            const record = getters.stocks.find(element => element.id == stock.id);
             return {
                 id: stock.id,
                 quantity: stock.quantity,
@@ -49,8 +49,8 @@ const getters = {
             }
         });
     },
-	funds(state) {
-        return state.funds;
+    funds (state) {
+      return state.funds;
     }
 };
 
