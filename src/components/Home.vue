98617660ed94cd0f03d0968d105030ad72718ae3
Yari Andres <yari.andres@gmail.com>
2021-02-02 22:12:10 +0100

    adding funds and displaying it

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-	<h1>The home component</h1>
+	<div class="">
+		<h1>Trade or view your Portfolio</h1>
+		<h6>You may save and load your Data</h6>
+		<h6>Click on 'End Day' to begin a New Day!</h6>
+		<hr />
+		<p>Your Funds: {{ funds }}</p>
+	</div>
 </template>
+
+<script>
+	export default {
+		computed: {
+			funds() {
+				return this.$store.getters.funds;
+			},
+		},
+	};
+</script>
